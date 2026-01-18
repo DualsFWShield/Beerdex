85fd88172ef699df264c445cef7ee4220c837d4a
Noah Bruijninckx <noah@bruijninckx.net>
2026-01-18 21:17:52 +0100

    Added new beer

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'Beerdex-v3'; // Increment to trigger update
+const CACHE_NAME = 'Beerdex-v4'; // Increment to trigger update
 const ASSETS = [
     './index.html',
     './style.css',
