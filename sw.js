ace9f8abb4947c23251c1930a5226cd244ee4253
Noah Bruijninckx <noah@bruijninckx.net>
2026-01-18 23:05:08 +0100

    VUpgrade

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'Beerdex-v8'; // Increment to trigger update
+const CACHE_NAME = 'Beerdex-v9'; // Increment to trigger update
 const ASSETS = [
     './index.html',
     './style.css',
